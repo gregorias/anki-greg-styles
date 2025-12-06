21786e3f8659a5d6e17dee2ac174ffdb4c8abafe
Grzegorz Milka <grzegorzmilka@gmail.com>
2025-12-06 12:55:02 +0100

    docs: use Google style

--- gregstyles/assets/guard.py
+++ gregstyles/assets/guard.py
@@ -1,4 +1,5 @@
 """This module handles guarding strings."""
+
 import re
 from typing import Tuple
 
@@ -15,41 +16,52 @@ def guard_comments(guard: str, comment_markup: Tuple[str, str]) -> Guards:
     Returns:
         The comment brackets.
     """
-    return (f'{comment_markup[0]} {guard} BEGIN {comment_markup[1]}\n',
-            f'{comment_markup[0]} {guard} END {comment_markup[1]}\n')
+    return (
+        f"{comment_markup[0]} {guard} BEGIN {comment_markup[1]}\n",
+        f"{comment_markup[0]} {guard} END {comment_markup[1]}\n",
+    )
 
 
 def guard_css_comments(guard: str) -> Guards:
     """
     Creates CSS comments bracketing import statements.
 
-    :param guard str A guard string used for CSS comments wrapping styles.
-    :rtype Tuple[str, str]
+    Args:
+        guard: A guard string used for CSS comments wrapping styles.
+
+    Returns:
+        The comment brackets.
     """
-    return guard_comments(guard, ('/*', '*/'))
+    return guard_comments(guard, ("/*", "*/"))
 
 
 def guard_html_comments(guard: str) -> Guards:
     """
     Creates HTML comments bracketing import statements.
 
-    :param guard str A guard string used for HTML comments wrapping the imports.
-    :rtype Tuple[str, str]
+    Args:
+        guard: A guard string used for HTML comments wrapping the imports.
+
+    Returns:
+        The comment brackets.
     """
-    return guard_comments(guard, ('<!--', '-->'))
+    return guard_comments(guard, ("<!--", "-->"))
 
 
 def append_guarded_snippet(tmpl: str, snippet: str, guards: Guards) -> str:
     """
     Appends a guarded snippet to a string.
 
-    :param tmpl str The string to modify.
-    :param snippet str The snippet to append.
-    :param guards Tuple[str, str] The guard strings.
-    :rtype str: The modified string.
+    Args:
+        tmpl: The string to modify.
+        snippet: The snippet to append.
+        guards: The guard strings.
+
+    Returns:
+        The modified string.
     """
     GUARD_BEGIN, GUARD_END = guards
-    gap = '\n' if tmpl.endswith('\n') else '\n\n'
+    gap = "\n" if tmpl.endswith("\n") else "\n\n"
     return tmpl + gap + GUARD_BEGIN + snippet + GUARD_END
 
 
@@ -57,11 +69,16 @@ def delete_guarded_snippet(tmpl: str, guards: Guards) -> str:
     """
     Deletes a guarded snippet from a string.
 
-    :param tmpl str The string to modify.
-    :param guards Tuple[str, str] The guard strings.
-    :rtype str: The modified string.
+    Args:
+        tmpl: The string to modify.
+        guards: The guard strings.
+
+    Returns:
+        The modified string.
     """
-    return re.sub(f'(\n)*{re.escape(guards[0])}.*{re.escape(guards[1])}',
-                  '\n',
-                  tmpl,
-                  flags=re.MULTILINE | re.DOTALL)
+    return re.sub(
+        f"(\n)*{re.escape(guards[0])}.*{re.escape(guards[1])}",
+        "\n",
+        tmpl,
+        flags=re.MULTILINE | re.DOTALL,
+    )
